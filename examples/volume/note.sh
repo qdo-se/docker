a4f532a736c90c010892932137248dc480972bbe
Quang Do <quang.do@colorado.edu>
2021-04-23 12:31:47 -0600

    Update

--- examples/volume/note.sh
+++ examples/volume/note.sh
@@ -7,7 +7,7 @@ docker run -it --rm --name alpine q/alpine /bin/sh
 
 # Mount volume
 docker run -it --rm -v /extra --name alpine q/alpine /bin/sh
-docker run -it --rm --mount type=volume,destination=/data --name alpine q/alpine /bin/sh
+docker run -it --rm --mount type=tmpfs,destination=/data --mount type=volume,destination=/data --name alpine q/alpine /bin/sh
 
 # Run and mount 2 additional anoonymous volumes
 docker run -it --rm --mount type=volume,destination=/data --mount type=volume,destination=/more --name alpine q/alpine /bin/sh
