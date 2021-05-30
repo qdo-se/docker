5845ceec24762b84b1c5499f13f5e51bd4ca6e91
Quang Do <quang.do.se@gmail.com>
2021-05-30 08:56:13 -0600

    Update

--- examples/pyenv/docker-entrypoint.sh
+++ examples/pyenv/docker-entrypoint.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-addgroup "${GROUP}" &>/dev/null
+addgroup "${GROUP}"
 
-useradd -g "${GROUP}" "${USER}" &>/dev/null
+useradd -m -d "/home/${USER}" -g "${GROUP}" "${USER}"
 
-cp -r /root/.pyenv/* /data/
+curl https://pyenv.run | bash \
+    && echo 'export PYENV_ROOT="$HOME/.pyenv"' >> "$HOME/.bashrc" \
+    && echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> "$HOME/.bashrc" \
+    && echo 'eval "$(pyenv init --path)"' >> "$HOME/.bashrc"
+
+# Run like an interactive shell since .bashrc ignores non-interactive commands
+/bin/bash -ic "source $HOME/.bashrc && pyenv install 3.4.10 && pyenv global 3.4.10"
+
+cp -r "$HOME"/.pyenv/* /data/
 
 chown -R "${USER}":"${GROUP}" /data
 
