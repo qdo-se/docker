89053191578c11905174d9e37ae3a59a4faa02cb
Quang Do <quang.do.se@gmail.com>
2021-05-30 11:14:28 -0600

    Update

--- examples/pyenv/docker-entrypoint.sh
+++ examples/pyenv/docker-entrypoint.sh
@@ -4,9 +4,9 @@ addgroup "${GROUP}" &>/dev/null
 
 useradd -m -d "/home/${USER}" -g "${GROUP}" "${USER}" &>/dev/null
 
-rm -rf "${HOME}/.pyenv" 2>/dev/null && mkdir "${HOME}/.pyenv"
+rm -rf "/home/${USER}/.pyenv" 2>/dev/null && mkdir "/home/${USER}/.pyenv"
 
-cp -r /root/.pyenv/* "${HOME}/.pyenv"
+cp -r /root/.pyenv/* "/home/${USER}/.pyenv"
 
 export PYENV_ROOT="${HOME}/.pyenv"
 export PATH="${PYENV_ROOT}/bin:$PATH"
@@ -15,7 +15,7 @@ eval "$(pyenv init --path)"
 pyenv install 3.4.10
 pyenv global 3.4.10
 
-cp -r "${HOME}/.pyenv"/* /sync
+cp -r "/home/${USER}/.pyenv"/* /sync
 
 chown -R "${USER}":"${GROUP}" /sync
 
